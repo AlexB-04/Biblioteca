f0e348417508e3bc188bd6b7bf7b4d4e56987575
AlexB-04 <bezverkhy04@gmail.com>
2026-06-30 16:04:23 +0100

    Implementado GET de Categorias na API

---
+++ Biblioteca/Biblioteca.API/Controllers/CategoriasController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Biblioteca.API.Controllers
+{
+    public class CategoriasController : ApiController
+    {
+        DataClasses1DataContext dc = new DataClasses1DataContext(
+            System.Configuration.ConfigurationManager
+            .ConnectionStrings["BibliotecaDBConnectionString"]
+            .ConnectionString);
+
+        // GET api/categorias
+        public List<Categoria> Get()
+        {
+            return dc.Categorias.ToList();
+        }
+
+        // GET api/categorias/1
+        public IHttpActionResult Get(int id)
+        {
+            Categoria categoria = dc.Categorias.FirstOrDefault(c => c.IdCategoria == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categoria);
+        }
+    }
+}
