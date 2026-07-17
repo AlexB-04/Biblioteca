baea015ec1a4f3a3b02c8b34f63adf3a1f6b1831
AlexB-04 <bezverkhy04@gmail.com>
2026-07-18 00:18:05 +0100

    Implementado GET de Reservas na API

---
+++ Biblioteca/Biblioteca.API/Controllers/ReservasController.cs
@@ -0,0 +1,58 @@
+﻿namespace Biblioteca.API.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    public class ReservasController : ApiController
+    {
+        DataClasses1DataContext dc = new DataClasses1DataContext(
+            System.Configuration.ConfigurationManager
+            .ConnectionStrings["BibliotecaDBConnectionString"]
+            .ConnectionString);
+
+        // GET api/reservas
+        public IHttpActionResult Get()
+        {
+            var reservas = dc.Reservas
+                .Select(r => new
+                {
+                    r.IdReserva,
+                    r.IdUtilizador,
+                    Utilizador = r.Utilizador.Nome,
+                    r.IdLivro,
+                    Livro = r.Livro.Titulo,
+                    r.DataReserva,
+                    r.Ordem,
+                    r.Ativa
+                })
+                .ToList();
+
+            return Ok(reservas);
+        }
+
+        // GET api/reservas/1
+        public IHttpActionResult Get(int id)
+        {
+            var reserva = dc.Reservas.Where(r => r.IdReserva == id)
+                .Select(r => new
+                {
+                    r.IdReserva,
+                    r.IdUtilizador,
+                    Utilizador = r.Utilizador.Nome,
+                    r.IdLivro,
+                    Livro = r.Livro.Titulo,
+                    r.DataReserva,
+                    r.Ordem,
+                    r.Ativa
+                })
+                .FirstOrDefault();
+
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reserva);
+        }
+    }
+}
